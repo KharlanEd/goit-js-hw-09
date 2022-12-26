67baeb164103c833a88de2ce8504718246553cfc
KharlanEd <109873871+KharlanEd@users.noreply.github.com>
2022-12-26 10:41:03 +0000

    Deploying to gh-pages from @ KharlanEd/goit-js-hw-09@94117754ba66a8e053ad672de7b548e6eb05fc62 🚀

---
+++ 01-color-switcher.c5f3d6a5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;e.disabled=!0;var a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.c5f3d6a5.js.map
